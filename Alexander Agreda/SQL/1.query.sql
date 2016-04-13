f20c7b025f1d3ec66b605280e08eb6515b5047fd
alexanderagreda <alexanderagreda@gmail.com>
2016-04-12 23:39:16 -0500

    Primeros consultas SQL World
    Primeros ejercicios de consulta SQL base de datos World

---
+++ Alexander Agreda/SQL/1.query.sql
@@ -0,0 +1,39 @@
+--Los países mas grandes del mundo
+SELECT 
+c.Name AS "Pais",
+c.SurfaceArea as "Area de superficie"
+FROM `country` c
+ORDER BY c.SurfaceArea DESC;
+
+--Los 10 países mas grandes del mundo
+SELECT 
+c.Name AS "Pais", 
+c.SurfaceArea AS "Area de superficie"
+FROM `country` c
+ORDER BY c.SurfaceArea DESC LIMIT 10;
+
+--Los 10 países mas ricos del mundo
+SELECT c.Name AS "Pais",
+c.GNP AS "PIB (Producto Interno Bruto)"
+FROM `country` c
+ORDER BY c.GNP DESC LIMIT 10;
+
+--Los 10 países más ricos con el PIB per Cápita
+SELECT 
+c.Name AS "Pais",
+c.GNP AS "PIB (Producto Interno Bruto)",
+c.GNP / c.Population as "PIB per capita"
+FROM `country` c 
+ORDER BY c.GNP DESC LIMIT 10;
+
+--Los 5 países con mayor expectativa de vida
+SELECT 
+c.Name AS "Pais",
+c.LifeExpectancy AS "Espectativa de vida"
+FROM `country` c
+ORDER BY c.LifeExpectancy DESC LIMIT 5;
+
+--El PIB de cada continente
+
+
+
